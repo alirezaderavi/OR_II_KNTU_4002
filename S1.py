f620bf679278fd719f122ad7de427aa51c265c49
Alireza <58109132+alirezaderavi@users.noreply.github.com>
2022-03-16 09:41:48 +0330

    Add files via upload

---
+++ S1.py
@@ -0,0 +1,21 @@
+print('salam')
+a='3.6'
+print(a)
+print('salam\n'+a+'ali\"')
+x='salam'
+x.upper()
+x.lower()
+len(x)
+x[3]
+abs(-2)
+pow(2,2)
+max(2,3,4)
+min(2,3,4)
+
+from math import *
+
+floor(3.4)
+ceil(3.4)
+sqrt(9)
+
+
